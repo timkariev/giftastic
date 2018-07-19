b714fa94cf0ac8663d35c5a78a5209bd7d35b94f
timkariev <tim.kariev95@gmail.com>
2018-07-18 20:35:17 -0700

    update1.0

---
+++ java.js
@@ -0,0 +1,19 @@
+function myFunction1() {
+    document.getElementById("demo").style.visibility = "visible";
+    document.getElementById("info").style.fontFamily = "papyrus";
+    document.getElementById("info").style.opacity = ".5";
+}
+function myFunction2() {
+    document.getElementById("container2").style.visibility = "visible";
+    document.getElementById("demo").style.opacity = ".5";
+    document.getElementById("info").fontFamily = "cursive";
+    $("#button1").css("pointer-events", "none");
+    $("#button2").css("pointer-events", "none");
+}
+function myFunction3() {
+    document.getElementById("container3").style.visibility = "visible";
+    document.getElementById("demo").style.opacity = ".5";
+    document.getElementById("info").fontFamily = "cursive";
+    $("#button1").css("pointer-events", "none");
+    $("#button2").css("pointer-events", "none");
+}
