5e7ebf1105474d37d8e9d5c6860a7749c4f870e8
timkariev <tim.kariev95@gmail.com>
2018-07-18 22:52:02 -0700

    upd1.1

--- assets/java/gif.js
+++ assets/java/gif.js
@@ -24,7 +24,7 @@ $(document).ready(function() {
     $(this).addClass("active");
 
     var type = $(this).attr("data-type");
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=dc6zaTOxFJmzC&limit=12";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=dc6zaTOxFJmzC&limit=12";
 
     $.ajax({
       url: queryURL,
